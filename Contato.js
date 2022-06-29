07608588affe817b25a69f0472f8f68617bffbe6
Lucas Manoel <102736187+LucasM1415@users.noreply.github.com>
2022-06-29 14:48:04 -0300

    Update Contato.js

--- Contato.js
+++ Contato.js
@@ -1,7 +1,6 @@
-
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import './Home.css';
+
 
 class Contato extends Component{
   render(){
@@ -9,11 +8,29 @@ class Contato extends Component{
       <div>
 
       <header>
-      <Link className="logo" to="/">Sistema</Link>
+      <Link className="logo" to="/Home">Sistema</Link>
       <Link className="cadastro" to="/Cadastro">Cadastro</Link>
       <Link className="contato" to="/Contato">Contato</Link>
       </header> 
-
+      <div className="Body">
+      <h2>Central de ajuda </h2>
+      <div className="Cajuda">
+        <p>Caso precise de alxilio, entre em contato nas nossas redes sociais:</p>
+        <div className="Links">
+          <Link className="linkf" to="/">Facebook</Link>
+          <Link className="link" to="/">Twiter</Link>
+          <Link className="link" to="/">Instagram</Link>
+        </div>
+      </div>
+        <footer>
+     <div className="Rodape"> 
+      <p>Instituto Federal de Pernambuco</p>
+      <p>Desenvolvimento Web 2</p>
+      <p>Palmares, 29/06/2022</p>
+    </div>
+        </footer>
+        
+      </div>
       </div>
 
     );
