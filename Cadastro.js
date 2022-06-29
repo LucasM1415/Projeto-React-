9a75b12a8903c669ee50f41adfb3dbdb6cac940d
Lucas Manoel <102736187+LucasM1415@users.noreply.github.com>
2022-06-29 09:25:16 -0300

    Update Cadastro.js

--- Cadastro.js
+++ Cadastro.js
@@ -1,1 +1,41 @@
 
+import React, { Component } from 'react';
+
+class Cadastro extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            nome:props.nome
+            
+        }
+        this.entrar = this.entrar.bind(this)
+    }
+
+    entrar(){
+       alert('Nome Alterado') 
+       this.setState({nome: 'Turma de IPI'})
+    }
+
+    render(){
+        return(
+            <div>
+
+               <h2>Bem-vindo(a) {this.state.nome}</h2>
+               <button onClick = {this.entrar}>
+
+                    Entrar como Jean
+
+               </button>
+               <button onClick ={() => this.setState({nome: ''})} >
+
+                    Sair
+
+               </button>
+
+            </div>
+        );
+    }
+}
+
+export default Cadastro;
