74293b921ebb2b3f31f8bd975e20af64409ab9cc
Lucas Manoel <102736187+LucasM1415@users.noreply.github.com>
2022-06-29 09:25:49 -0300

    Update Contato.js

--- Contato.js
+++ Contato.js
@@ -1,1 +1,24 @@
 
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+class Contato extends Component{
+  render(){
+    return(
+      <div>
+
+      <header>
+      <Link className="logo" to="/">Sistema</Link>
+      <Link className="cadastro" to="/Cadastro">Cadastro</Link>
+      <Link className="contato" to="/Contato">Contato</Link>
+      </header> 
+
+      </div>
+
+    );
+  }
+}
+
+
+export default Contato;
