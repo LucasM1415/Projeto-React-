9b5d02ceffd49f08d1aa32b82da242491a53819f
Lucas Manoel <102736187+LucasM1415@users.noreply.github.com>
2022-07-03 13:00:33 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
-
-import React, { Component } from 'react';
-import RoutesApp from './routes';
-
-class App extends Component{
-  render(){
-    return(
-
-      <RoutesApp/>
-
-    );
-  }
+import React, { Component } from "react";
+import RoutesApp from "./routes";
+class App extends Component {
+ render() {
+   return <RoutesApp />;
+ }
 }
-
-
+ 
 export default App;
+ 
+
+
+
