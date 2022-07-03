48a49e2b51aac8759c8f1dae7fca3eb533867710
Lucas Manoel <102736187+LucasM1415@users.noreply.github.com>
2022-07-03 13:02:08 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,26 +1,24 @@
 
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
  
-import Home from './pages/Home';
-import Cadastro from './pages/Cadastro';
-import Contato from './pages/Contato';
+import Home from "./pages/Home";
+import Cadastro from "./pages/Cadastro";
+import Contato from "./pages/Contato";
+import Login from "./pages/Login";
  
- 
-function RoutesApp(){
-   return(
-       <BrowserRouter>
-       <Routes>
-           <Route path ="/" element = {<Home/>} />
-           <Route path ="/cadastro" element = {<Cadastro/>} />
-           <Route path ="/Contato" element = {<Contato/>} />
-       </Routes>
-       </BrowserRouter>
-   )
- 
-  
- 
- 
+function RoutesApp() {
+ return (
+   <BrowserRouter>
+     <Routes>
+       <Route path="/Home" element={<Home />} />
+       <Route path="/Cadastro" element={<Cadastro />} />
+       <Route path="/Contato" element={<Contato />} />
+       <Route path="/Login" element={<Login />} />
+     </Routes>
+   </BrowserRouter>
+ );
 }
  
 export default RoutesApp;
+ 
+
